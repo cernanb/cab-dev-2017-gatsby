67b1dab611f9a0803c2e1207bbcdb2a7751f66b3
Cernan Bernardo <cernanb@gmail.com>
2017-12-20 21:12:39 -0800

    Update typewriter component

--- src/components/Typewriter.js
+++ src/components/Typewriter.js
@@ -4,21 +4,34 @@ import Typist from 'react-typist'
 class Typewriter extends Component {
   state = {
     typing: true,
-    message: 'build websites'
+    messages: [
+      'build websites',
+      'love code',
+      'drink coffee',
+      'enjoy javascript'
+    ]
   }
   done = () => {
     setTimeout(() => {
       this.setState({ typing: false }, () => {
-        this.setState({ typing: true, message: 'drink coffee' })
-      });
+        this.setState({ typing: true })
+      })
     }, 2000)
   }
   render() {
-    return (
-      this.state.typing
-      ? <Typist startDelay={1000} cursor={{show: false}} onTypingDone={this.done}>{this.state.message}</Typist>
+    return this.state.typing
+      ? <Typist
+          startDelay={1000}
+          cursor={{ show: false }}
+          onTypingDone={this.done}
+        >
+          {
+            this.state.messages[
+              Math.floor(Math.random() * this.state.messages.length)
+            ]
+          }
+        </Typist>
       : null
-    )
   }
 }
 

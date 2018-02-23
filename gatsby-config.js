b39486ba99c83504ee70bef0620b7e6ebf208b1d
Cernan Bernardo <cernanb@gmail.com>
2018-02-23 00:24:46 -0800

    Configure netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: 'markdown-pages',
+      },
+    },
+  ],
 }
